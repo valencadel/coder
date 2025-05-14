663acfa79e423f9ef18c7063f30180a999e60b95
Valentin Cadel <valencadel@gmail.com>
2025-05-14 14:42:42 -0300

    clase 3 pruebas

---
+++ clase_3/js/app.js
@@ -0,0 +1,9 @@
+// Helper function to display output
+function display(content) {
+    const outputDiv = document.getElementById('output');
+    outputDiv.innerHTML += `<div>${content}</div>`;
+}
+
+// Example usage
+console.log("JavaScript file connected!");
+display("JavaScript is ready for testing!"); 
