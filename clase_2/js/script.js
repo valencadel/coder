e0dcb57a9170eda2327deb11eb9d757d1b7335d7
Valentin Cadel <valencadel@gmail.com>
2025-05-14 14:30:23 -0300

    clase 2

--- clase_2/js/script.js
+++ clase_2/js/script.js
@@ -1,5 +1,33 @@
-const parrafo = document.getElementById("parrafo");
-parrafo.textContent = "Parrafo modificado";
+const user = 'valentin';
+const password = '1234';
+let intentos = 3;
+let ingreso = false;
+
+// for (let i = 0; i < intentos; i++) {
+//   let userIngresado = prompt('Ingrese su usuario');
+//   let passwordIngresado = prompt('Ingrese su contraseña');
+
+//   if (userIngresado === user && passwordIngresado === password) {
+//     alert('Bienvenido');
+//   } else if (userIngresado === user && passwordIngresado !== password) {
+//     alert('La contraseña es incorrecta');
+//   } else if (userIngresado !== user && passwordIngresado === password) {
+//     alert('El usuario es incorrecto');
+//   } else {
+//     alert('Usuario o contraseña incorrectos');
+//   }
+// };
+
+let passwordIngresado = prompt('Ingrese su contraseña');
+
+while (passwordIngresado != password) {
+  passwordIngresado = prompt('Ingrese su contraseña');
+  if (passwordIngresado === password) {
+    alert('Bienvenido');
+  } else {
+    alert('La contraseña es incorrecta');
+  }
+}
 
 const mensaje = document.getElementById("mensaje");
 
@@ -7,3 +35,4 @@ let descuentoInicial;
 
 const descuentoPredeterminado = 10;
 let descuentoAplicado = descuentoInicial ?? descuentoPredeterminado;
+mensaje.textContent = `El descuento es del ${descuentoAplicado}%`;
