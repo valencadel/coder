c7207fa6d4702955a1b93f4069ecabf54902d8af
Valentin Cadel <valencadel@gmail.com>
2025-11-25 23:52:23 -0300

    fin clase 6 con mejoras

--- back_1/clase_6/src/app.js
+++ back_1/clase_6/src/app.js
@@ -29,14 +29,30 @@ const httpServer = app.listen(PORT,() => { //esto va a devolver una instancia de
 
 const serverSocket = new Server(httpServer) // creo un servidor de socket.io
 
+const BBDD_CHAT = [];
+
 serverSocket.on('connection', (socket) => { // cuando un cliente se conecta, se ejecuta esta funcion, el metodo on es para escuchar eventos. Hay eventos por defecto, como el connection, que hace referncia a cada vez que un cliente nuevo se conecta
   console.log('nuevo cliente conectado ->', socket.id)
   
-  socket.on('mensaje', (data) => { // el servidor debe estar escuchando el evento que envia el cliente.
-    console.log(data)
+  // socket.on('mensaje', (data) => { // el servidor debe estar escuchando el evento que envia el cliente.
+  //   console.log(data)
     
-    serverSocket.emit('respuesta_mensaje', 'Un usuario me saludó desde el servidor')
+  //   serverSocket.emit('respuesta_mensaje', 'Un usuario me saludó desde el servidor')
     // cuando el servidor emite, el cliente debe estar escuchando dicho evento.
-  })
+  // })
 
+  socket.on('usuario_conectado', (data) => {
+    console.log('nuevo usuario conectado ->', data.username)
+    // Una vez que el usuario se registra, enviamos el historial de mensajes
+    socket.emit('lista_mensajes', BBDD_CHAT);
+  });
+
+  socket.on('mensaje', (data) => { // data es el objeto que envia el cliente -> { username: 'nombre', message: 'mensaje' }
+    const messageWithTimestamp = {
+      ...data,
+      timestamp: new Date().toLocaleTimeString()
+    };
+    BBDD_CHAT.push(messageWithTimestamp);
+    serverSocket.emit('lista_mensajes', BBDD_CHAT);
+  })
 })
--- back_1/clase_6/src/public/js/index.js
+++ back_1/clase_6/src/public/js/index.js
@@ -1,9 +1,60 @@
 console.log('este se ve por consola en pantalla')
 const socket = io();
+const box = document.getElementById('box');
+const appChat = document.getElementById('app-chat');
+const teclaEnter = 'Enter';
 
-socket.emit('mensaje', 'Hola desde el cliente'); // el cliente esta emitiendo el mensaje para que el servidor lo escuche.
+// socket.emit('mensaje', 'Hola desde el cliente'); // el cliente esta emitiendo el mensaje para que el servidor lo escuche.
 
-socket.on('respuesta_mensaje', (data) => {
-  console.log('el servidor me envio el evento respuesta_mensaje con esta info:', data)
-  // el cliente (index.js) esta escuchando el evento respuesta_mensaje que envia el servidor (app.js) y cuando lo recibe, ejecuta la funcion que le pasamos como segundo parametro.
+// socket.on('respuesta_mensaje', (data) => {
+//   console.log('el servidor me envio el evento respuesta_mensaje con esta info:', data)
+//   // el cliente (index.js) esta escuchando el evento respuesta_mensaje que envia el servidor (app.js) y cuando lo recibe, ejecuta la funcion que le pasamos como segundo parametro.
+// })
+
+let username = '';
+
+Swal.fire({
+  title: '¿Quien eres?',
+  input: 'text',
+  text: 'Escribe tu nombre',
+  allowOutsideClick: false,
+  inputValidator: (value) => {
+    if (!value) {
+      return 'Debes escribir un nombre'
+    }
+    return null;
+  }
+}).then((result) => {
+  username = result.value;
+
+  const welcomeMessage = document.getElementById('welcome-message');
+  if (welcomeMessage) {
+    welcomeMessage.textContent = `Bienvenid@ al chat ${username}!`;
+  }
+
+  socket.emit('usuario_conectado', { username });
 })
+
+box.addEventListener('keyup', (event) => {
+  const { key, target } = event;
+  if (key === teclaEnter && target.value.trim() !== '') {
+    socket.emit('mensaje', { // el cliente esta emitiendo el mensaje para que el servidor lo escuche.
+      username: username,
+      message: target.value
+    });
+    box.value = ''; // limpia el input
+  }
+});
+
+socket.on('lista_mensajes', (data) => {
+  console.log('el servidor me envio el evento lista_mensajes con esta info:', data)
+  // el cliente (index.js) esta escuchando el evento lista_mensajes que envia el servidor (app.js) y cuando lo recibe, ejecuta la funcion que le pasamos como segundo parametro.
+  
+  appChat.innerHTML = '';
+  // limpio el chat para mostrar "solo" los mensajes nuevos
+  data.forEach(chatMessage => {
+    const p = document.createElement('p');
+    p.innerText = `[${chatMessage.timestamp}] ${chatMessage.username}: ${chatMessage.message}`;
+    appChat.appendChild(p);
+  });
+});
